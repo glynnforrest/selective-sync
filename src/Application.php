90bcc685e3e890472a6b7d4de1608f7dc3290ad7
Glynn Forrest <me@glynnforrest.com>
2015-05-14 23:26:28 +0100

    Adding single command Application.

---
+++ src/Application.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace SelectiveSync;
+
+use SelectiveSync\Command\SyncCommand;
+use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Input\InputInterface;
+
+/**
+ * Application
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class Application extends BaseApplication
+{
+    /**
+     * Gets the name of the command based on input.
+     *
+     * @param InputInterface $input The input interface
+     *
+     * @return string The command name
+     */
+    protected function getCommandName(InputInterface $input)
+    {
+        return 'sync';
+    }
+
+    /**
+     * Gets the default commands that should always be available.
+     *
+     * @return array An array of default Command instances
+     */
+    protected function getDefaultCommands()
+    {
+        // Keep the core default commands to have the HelpCommand
+        // which is used when using the --help option
+        $defaultCommands = parent::getDefaultCommands();
+
+        $defaultCommands[] = new SyncCommand();
+
+        return $defaultCommands;
+    }
+
+    /**
+     * Overridden so that the application doesn't expect the command
+     * name to be the first argument.
+     */
+    public function getDefinition()
+    {
+        $inputDefinition = parent::getDefinition();
+        // clear out the normal first argument, which is the command name
+        $inputDefinition->setArguments();
+
+        return $inputDefinition;
+    }
+}
